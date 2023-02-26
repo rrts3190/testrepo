b3e61f4dec315d9fc3f63205742e057a1858e902
rrts3190 <Ramesh@3190>
2023-02-26 11:52:59 +0530

    update project Multiple DB in spring boot

--- multipldb/src/main/java/com/multipldb/multipldb/postgresql/entities/Product.java
+++ multipldb/src/main/java/com/multipldb/multipldb/postgresql/entities/Product.java
@@ -1,11 +1,20 @@
 package com.multipldb.multipldb.postgresql.entities;
 
 
-import javax.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
-@Table(name = "entities")
+@Table(name = "products")
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
