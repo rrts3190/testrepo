b1f4b98aaa199ecabbd96d8a0056d06c9b636fe0
rrts3190 <Ramesh@3190>
2023-03-05 21:46:17 +0530

    Hibernate Tutorial with merge & update

--- HibernateTutorial/src/main/java/org/tutorial/MergeUpdateDiff.java
+++ HibernateTutorial/src/main/java/org/tutorial/MergeUpdateDiff.java
@@ -11,17 +11,11 @@ public class MergeUpdateDiff
         SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
         Session session1 = sessionFactory.openSession();
         Student student1 = session1.get(Student.class, 1);
-
-        Transaction tx = session1.beginTransaction();
-        tx.commit();
         session1.close();
 
-
-        //Thread.sleep(3000);
         Session session2 = sessionFactory.openSession();
         Student student2 = session2.get(Student.class, 1);
         Transaction tx2 = session2.beginTransaction();
-        //student2.setName("tiwari6");
         student1.setName("tiwari60");
         session2.update(student1); // org.hibernate.NonUniqueObjectException
        // session2.merge(student1); //run successfully
